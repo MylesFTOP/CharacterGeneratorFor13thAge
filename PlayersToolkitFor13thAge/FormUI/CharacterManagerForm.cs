cbc0b66fd53098b00ec77bf562cad8dd79b404c3
MylesFTOP <m.fullen87@gmail.com>
2020-06-08 23:40:27 +0100

    Moving files to folders to tidy structure; beginning work for saving data to text files

--- PlayersToolkitFor13thAge/FormUI/CharacterManagerForm.cs
+++ PlayersToolkitFor13thAge/FormUI/CharacterManagerForm.cs
@@ -39,6 +39,8 @@ namespace FormUI
                 output = false;
             }
 
+
+
             return output;
         }
 
---
+++ PlayersToolkitFor13thAge/ToolkitLibrary/DataAccess/TextConnector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToolkitLibrary
+{
+    public class TextConnector : IDataConnection
+    {
+        public CharacterModel SaveCharacter(CharacterModel character) {
+            // Load the text file
+            // Convert the text to a list<CharacterModel>
+            // Read the characters to find max ID
+            // Add the new record with the new ID (max + 1)
+            // Convert the character to a list<string>
+            // Save the list<string> to the text file
+
+        }
+    }
+}
