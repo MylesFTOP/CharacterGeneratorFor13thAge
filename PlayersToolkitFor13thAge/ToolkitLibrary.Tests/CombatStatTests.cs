a56b44dcaa62fa053b7d4657b2f00d54f1373c9d
MylesFTOP <m.fullen87@gmail.com>
2020-06-14 18:09:19 +0100

    Adding PD and MD calculation to CombatStat, setting up class for unit tests on CombatStat methods

---
+++ PlayersToolkitFor13thAge/ToolkitLibrary.Tests/CombatStatTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ToolkitLibrary.Tests
+{
+    public class CombatStatTests
+    {
+        private readonly CombatStat combatStat = new CombatStat();
+
+        [Fact]
+        public void CombatStat_ArmorClassShouldCalculate() {
+
+        }
+
+        [Fact]
+        public void CombatStat_PhysicalDefenseShouldCalculate() {
+
+        }
+
+        [Fact]
+        public void CombatStat_MentalDefenseShouldCalculate() {
+
+        }
+    }
+}
--- PlayersToolkitFor13thAge/ToolkitLibrary/Models/CombatStat.cs
+++ PlayersToolkitFor13thAge/ToolkitLibrary/Models/CombatStat.cs
@@ -9,16 +9,28 @@ namespace ToolkitLibrary
     public class CombatStat
     {
         public int CombatStatValue { get; private set; }
+        // TODO: Add lookup for base value from character class to then pass to argument
 
         public static int CalculateArmorClass(CharacterModel character) {
-            // TODO: Add lookup for base value from character class to then pass to argument
-            int baseValue = 16;
+            int baseValue = 10; // Hard-coded value for Sorcerer - see todo above
 
             int armorClass = CalculateCombatStat(baseValue, character.Constitution, character.Dexterity, character.Wisdom);
             return armorClass;
         }
 
-        // TODO: Add methods to calculate PD and MD
+        public static int CalculatePhysicalDefense(CharacterModel character) {
+            int baseValue = 11; // Hard-coded value for Sorcerer - see todo above
+
+            int physicalDefense = CalculateCombatStat(baseValue, character.Strength, character.Constitution, character.Dexterity);
+            return physicalDefense;
+        }
+
+        public static int CalculateMentalDefense(CharacterModel character) {
+            int baseValue = 10; // Hard-coded value for Sorcerer - see todo above
+
+            int mentalDefense = CalculateCombatStat(baseValue, character.Intelligence, character.Wisdom, character.Charisma);
+            return mentalDefense;
+        }
 
         private static int CalculateCombatStat(int classBaseValue, uint firstAbilityStat, uint secondAbilityStat, uint thirdAbilityStat) {
             uint[] abilityStats = { firstAbilityStat, secondAbilityStat, thirdAbilityStat };
