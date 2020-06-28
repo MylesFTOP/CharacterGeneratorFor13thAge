6753bf9567cc0919c050ce07230006b852be6f36
MylesFTOP <m.fullen87@gmail.com>
2020-06-28 19:04:14 +0100

    Adding initial version of dice to project (technically, it should be "Die" to be singular, but that sounds rather dramatic)

---
+++ PlayersToolkitFor13thAge/ToolkitLibrary/Dice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolkitLibrary
+{
+    public class Dice
+    {
+        public uint DiceRoll { get; private set; }
+
+        public uint RollDice() {
+            Random random = new Random();
+            uint diceValue = (uint)random.Next(1, 6);
+            return diceValue;
+        }
+    }
+}
