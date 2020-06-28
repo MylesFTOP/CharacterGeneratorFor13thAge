f8ac8f1d583c0042c13e8caf16259a364a246889
MylesFTOP <m.fullen87@gmail.com>
2020-06-28 18:35:36 +0100

    Adding new class for random character generation

---
+++ PlayersToolkitFor13thAge/ToolkitLibrary/CharacterGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolkitLibrary
+{
+    public class CharacterGenerator
+    {
+        Random rand = new Random();
+
+        public uint generateRandomAbilityStat() {
+            uint randomAbilityStat = (uint)rand.Next(5,20);
+            return randomAbilityStat;
+        }
+
+    }
+}
