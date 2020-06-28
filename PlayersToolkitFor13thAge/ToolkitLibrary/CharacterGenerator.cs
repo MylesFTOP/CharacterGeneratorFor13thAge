fb5bd85005ae170421138aae9c36cb70dfd022a6
MylesFTOP <m.fullen87@gmail.com>
2020-06-28 20:37:10 +0100

    Removed unused references

--- PlayersToolkitFor13thAge/ToolkitLibrary/CharacterGenerator.cs
+++ PlayersToolkitFor13thAge/ToolkitLibrary/CharacterGenerator.cs
@@ -7,6 +7,7 @@ namespace ToolkitLibrary
         readonly Random rand = new Random();
 
         public uint GenerateRandomAbilityStat() {
+            // TODO: Change to use Dice.RollAbilityStat() 4 times and drop the lowest
             uint randomAbilityStat = (uint)rand.Next(5,20);
             return randomAbilityStat;
         }
