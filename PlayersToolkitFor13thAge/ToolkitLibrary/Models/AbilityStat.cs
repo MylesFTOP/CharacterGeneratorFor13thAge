bc40466ef1035b7421cfb36b82e219a7f032efb3
MylesFTOP <m.fullen87@gmail.com>
2020-08-16 18:04:14 +0100

    Removing unused code

--- PlayersToolkitFor13thAge/ToolkitLibrary/Models/AbilityStat.cs
+++ PlayersToolkitFor13thAge/ToolkitLibrary/Models/AbilityStat.cs
@@ -6,10 +6,6 @@ namespace ToolkitLibrary
     {
         public uint AbilityStatValue { get; set; }
 
-        public AbilityStatType AbilityStatName { get; private set; }
-        public int Modifier { get; private set; }
-        public int ModifierWithLevel { get; private set; }
-
         public int CalculateModifier(uint abilityStat) {
             double modifier = Math.Floor(((double)abilityStat - 10) / 2);
             return (int)modifier;
@@ -30,15 +26,6 @@ namespace ToolkitLibrary
 
         public AbilityStat(string inputValue) {
             AbilityStatValue = uint.Parse(inputValue);
-        }
-
-        public enum AbilityStatType {
-            strength,
-            constitution,
-            dexterity,
-            wisdom,
-            intelligence,
-            charisma
         }
     }
 }
