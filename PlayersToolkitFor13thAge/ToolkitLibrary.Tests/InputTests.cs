bb5e5bdbb422395d55fa100fb7c02fd6a0aa75b1
MylesFTOP <m.fullen87@gmail.com>
2020-09-17 00:34:47 +0100

    Beginning to extract validation logic to InputHandler (currently duplicated between int and uint while moving over to exclusively int)

---
+++ PlayersToolkitFor13thAge/ToolkitLibrary.Tests/InputTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ToolkitLibrary.Tests
+{
+    public class InputTests
+    {
+        [Theory]
+        [InlineData(false, "a")]
+        [InlineData(true, "3")]
+        public void InputHandler_ValidateValueAsIntShouldReturnBool(bool expectedValue, string input) {
+            bool actualValue = InputHandler.ValidateValueAsInt(input, out int result);
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Theory]
+        [InlineData(false, "a")]
+        [InlineData(true, "3")]
+        public void InputHandler_ValidateValueAsUintShouldReturnBool(bool expectedValue, string input) {
+            bool actualValue = InputHandler.ValidateValueAsUint(input, out uint result);
+            Assert.Equal(expectedValue, actualValue);
+        }
+    }
+}
---
+++ PlayersToolkitFor13thAge/ToolkitLibrary/Services/InputHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolkitLibrary
+{
+    public static class InputHandler
+    {
+        public static bool ValidateValueAsInt(string stringInput, out int result) {
+            bool output = int.TryParse(stringInput, out result);
+            return output;
+        }
+        
+        public static bool ValidateValueAsUint(string stringInput, out uint result) {
+            bool output = uint.TryParse(stringInput, out result);
+            return output;
+        }
+    }
+}
