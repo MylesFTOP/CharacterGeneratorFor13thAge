a0257242b8a54d649ea83c61263d18b1f64c6558
MylesFTOP <m.fullen87@gmail.com>
2020-09-17 21:30:07 +0100

    Add tests for ParseNonNegativeInt()

--- PlayersToolkitFor13thAge/ToolkitLibrary.Tests/InputTests.cs
+++ PlayersToolkitFor13thAge/ToolkitLibrary.Tests/InputTests.cs
@@ -11,9 +11,19 @@ namespace ToolkitLibrary.Tests
     {
         [Theory]
         [InlineData(false, "a")]
+        [InlineData(true, "-1")]
         [InlineData(true, "3")]
         public void InputHandler_ParseIntShouldReturnBool(bool expectedValue, string input) {
             bool actualValue = InputHandler.ParseInt(input, out int result);
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Theory]
+        [InlineData(false, "a")]
+        [InlineData(false, "-1")]
+        [InlineData(true, "3")]
+        public void InputHandler_ParseNonNegativeIntShouldReturnBool(bool expectedValue, string input) {
+            bool actualValue = InputHandler.ParseNonNegativeInt(input, out int result);
             Assert.Equal(expectedValue, actualValue);
         }
 
