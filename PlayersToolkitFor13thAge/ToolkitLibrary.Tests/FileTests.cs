ac2d3bf4ce114135291dbac1c1537ba975802445
MylesFTOP <m.fullen87@gmail.com>
2021-02-22 01:05:51 +0000

    Adding failing test on FullFilePath before setting up path on config file; excluding config file in gitignore

---
+++ PlayersToolkitFor13thAge/ToolkitLibrary.Tests/FileTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mime;
+using Xunit;
+using ToolkitLibrary.DataAccess.TextHelpers;
+
+namespace ToolkitLibrary.Tests
+{
+    public class FileTests
+    {
+        private const string file = "CharacterModels.csv";
+        private readonly ContentType contentType = new ContentType();
+
+        [Fact]
+        public void TextConnectorProcessor_FullFilePath_ShouldReturnValidPath() {
+            var expected = true;
+            var actual = file
+                .FullFilePath()
+                .Contains($"C:\\");
+            Assert.Equal(expected, actual);
+        }
+    }
+}
